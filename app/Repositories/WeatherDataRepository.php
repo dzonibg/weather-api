c9d1664fb2cd425eb73442c7be38037c0d11ac3f
Nikola <nidza.bg@gmail.com>
2022-03-27 04:13:39 +0200

    Quick fixes.

--- app/Repositories/WeatherDataRepository.php
+++ app/Repositories/WeatherDataRepository.php
@@ -14,7 +14,8 @@ class WeatherDataRepository {
             'city_id' => $cityID,
             'time' => date('Y-m-d H:i:s'),
             'service_id' => $serviceID,
-            'temperature' => $temperature
+            'temperature' => $temperature,
+            'created_at' => date('Y-m-d H:i:s')
         ]);
 
         return $query;
@@ -24,6 +25,7 @@ class WeatherDataRepository {
     public function getCurrentDataByCityId($city_id) {
         $data = DB::table('weather_data')
             ->orderBy('time', 'desc')
+            ->where('city_id', $city_id)
             ->limit(1)
             ->first(['time', 'city_id', 'service_id', 'created_at', 'temperature']);
         return $data;
