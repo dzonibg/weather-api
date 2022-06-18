0a4cf2d9866785ea78bbb76f9cc0e5fbb81007c9
Nikola <nidza.bg@gmail.com>
2022-06-18 06:21:20 +0200

    Added city cmr. Need that base API controller.

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\CityRepository;
+use Illuminate\Http\Request;
+
+class CityController extends Controller
+{
+    public function index(CityRepository $cityRepository) {
+        $data = $cityRepository->index();
+        return response()->json(['data' => $data], 200);
+    }
+
+    public function create(CityRepository $cityRepository) {
+        $data = request();
+        try {
+
+            $data = $this->validate($data, [
+                'name' => 'string|required',
+                'country_id' => 'int'
+            ]);
+
+            $response = $cityRepository->create($data);
+
+            return response()->json(['data' => $data], 201);
+
+        } catch (\RuntimeException $exception) {
+            return response()->json(['data' => $exception->getMessage()]);
+        }
+    }
+}
---
+++ app/Repositories/CityRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\City;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class CityRepository {
+
+    private string $table = "cities";
+    private $model = null;
+
+    public function __construct() {
+        $this->model = new City();
+    }
+
+    public function index() {
+        return DB::table('cities')
+            ->select()
+            ->orderBy('id', 'asc')
+            ->get();
+    }
+
+    public function create($data) {
+        return DB::table('cities')
+            ->insert([
+                'name',
+                'country_id',
+                'created_at' => Carbon::now()
+            ]);
+    }
+
+    public function show($cityID) {
+        return DB::table('cities')
+            ->select()
+            ->where('city_id', $cityID)
+            ->get();
+    }
+}
