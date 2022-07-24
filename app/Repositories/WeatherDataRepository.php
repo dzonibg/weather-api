ae8c88edad8ce0311caf6b13e29684d72aeebf86
Nikola <nidza.bg@gmail.com>
2022-07-24 04:36:40 +0200

    Changing updated_at to "time" for timezone compatibility

--- app/Repositories/WeatherDataRepository.php
+++ app/Repositories/WeatherDataRepository.php
@@ -45,11 +45,11 @@ class WeatherDataRepository {
     public function getLastDayAveragePerHour($city_id) {
         $query = "
         with temperature_data as
-                         (select city_id, temperature, date_trunc('hour', created_at), row_number() over (PARTITION BY date_trunc('hour', created_at)) rn
+                         (select city_id, temperature, date_trunc('hour', time), row_number() over (PARTITION BY date_trunc('hour', time)) rn
                           from weather_data
-                          where created_at >= now() - interval '1 DAY'
-                          group by city_id, temperature, date_trunc('hour', created_at)
-                          order by date_trunc('hour', created_at) asc)
+                          where time >= now() - interval '1 DAY'
+                          group by city_id, temperature, date_trunc('hour', time)
+                          order by date_trunc('hour', time) asc)
         select round(avg(temperature)::numeric, 1), date_trunc::time from temperature_data
         group by date_trunc
         ";
